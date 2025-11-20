0515668ddfbddf9e638a7af8d64c50047df89611
scmdcunha <saracmc21@gmail.com>
2025-11-20 17:17:43 +0000

    Add initial script to run all scripts

---
+++ 05_scripts/run_pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "Starting pipeline..."
+
+# 1. Running fastqc in raw data
+echo "Step 1: FastQC RAW"
+bash 05_scripts/run_fastqc.sh 01_data 03_results/fastqc_raw
+
+# 2. MultiQC in raw data
+# echo "Step 2: MultiQC RAW"
+# bash 05_scripts/run_multiqc.sh 03_results/fastqc_raw
+
+# 3. Trimmomatic
+# echo "Step 3: Trimmomatic"
+# bash 05_scripts/run_trimmomatic.sh
+
+# 4. FastQC on trimmed reads
+# echo "Step 4: FastQC TRIMMED "
+# bash 05_scripts/run_fastqc.sh 03_results/trimmed 03_results/fastqc_trimmed
+
+# 5. MultiQC final
+# echo "Step 5: MultiQC FINAL"
+# bash 05_scripts/run_multiqc.sh
+
+# echo "Pipeline completed successfully!"
